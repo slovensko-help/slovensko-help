4771cfe81a2d966e4808713b309b97232c8f97e7
“Filip <filip@bratia.sk>
2021-01-19 18:14:31 +0100

    Add http2imap

---
+++ tools/http2imap/config.default.php
@@ -0,0 +1,3 @@
+<?php
+
+define('AUTH_TOKEN', 'securestring');
---
+++ tools/http2imap/get.php
@@ -0,0 +1,54 @@
+<?php
+
+include_once 'config.php';
+
+function response($payload) {
+    header('Content-Type: application/json');
+    echo json_encode($payload);
+    exit;
+}
+
+function errorResponse($error) {
+    response([
+        'success' => false,
+        'error' => $error,
+    ]);
+}
+
+$mailbox = isset($_POST['mailbox']) ? $_POST['mailbox'] : null;
+$user = isset($_POST['user']) ? $_POST['user'] : null;
+$password = isset($_POST['password']) ? $_POST['password'] : null;
+$subject = isset($_POST['subject']) ? $_POST['subject'] : null;
+$token = isset($_POST['token']) ? $_POST['token'] : null;
+
+
+if (null === $mailbox || null === $user || null === $password || null === $token || null === $subject) {
+    errorResponse('Wrong parameter values.');
+}
+
+if ($token !== AUTH_TOKEN) {
+    errorResponse('Wrong auth token.');
+}
+
+$mailbox = imap_open($mailbox,$user,$password);
+
+if (false === $mailbox) {
+    errorResponse(imap_last_error());
+}
+
+$MC = imap_check($mailbox);
+
+$messageUids = imap_search($mailbox, 'SUBJECT "' . $subject . '" SINCE "' . (new DateTimeImmutable('3 days ago'))->format('j F Y') . '"', SE_UID);
+
+$result = [
+    'success' => true,
+    'messages' => [],
+];
+
+foreach ($messageUids as $messageUid) {
+    $result['messages'][] = imap_fetchbody($mailbox, $messageUid, "");
+}
+
+imap_close($mailbox);
+
+response($result);
