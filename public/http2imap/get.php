b132024f02384eacc2791230147818a686d04c57
“Filip <filip@bratia.sk>
2021-01-22 17:26:36 +0100

    Refactor http2imap proxy

--- public/http2imap/get.php
+++ public/http2imap/get.php
@@ -1,221 +1,208 @@
 <?php
 
+namespace http2imap;
+
+use DateTimeImmutable;
+use Exception;
+
 include_once 'config.php';
 
-class mail {
-    /**
-     * If you change one of these, please check the other for fixes as well
-     *
-     * @const Pattern to match RFC 2047 charset encodings in mail headers
-     */
-    const rfc2047header = '/=\?([^ ?]+)\?([BQbq])\?([^ ?]+)\?=/';
+class Controller
+{
+    const PARAMETER_REQUIRED = 1;
+    const PARAMETER_OPTIONAL = 1;
 
-    const rfc2047header_spaces = '/(=\?[^ ?]+\?[BQbq]\?[^ ?]+\?=)\s+(=\?[^ ?]+\?[BQbq]\?[^ ?]+\?=)/';
+    private $request = [
+        'mailbox' => self::PARAMETER_REQUIRED,
+        'user' => self::PARAMETER_REQUIRED,
+        'password' => self::PARAMETER_REQUIRED,
+        'subject' => self::PARAMETER_REQUIRED,
+        'token' => self::PARAMETER_REQUIRED,
+        'since' => self::PARAMETER_OPTIONAL,
+        'before' => self::PARAMETER_OPTIONAL,
+    ];
+    private string $token;
 
-    /**
-     * http://www.rfc-archive.org/getrfc.php?rfc=2047
-     *
-     * =?<charset>?<encoding>?<data>?=
-     *
-     * @param string $header
-     */
-    public static function is_encoded_header($header) {
-        // e.g. =?utf-8?q?Re=3a=20Support=3a=204D09EE9A=20=2d=20Re=3a=20Support=3a=204D078032=20=2d=20Wordpress=20Plugin?=
-        // e.g. =?utf-8?q?Wordpress=20Plugin?=
-        return preg_match(self::rfc2047header, $header) !== 0;
+    public function __construct(string $token)
+    {
+        $this->token = $token;
     }
 
-    public static function header_charsets($header) {
-        $matches = null;
-        if (!preg_match_all(self::rfc2047header, $header, $matches, PREG_PATTERN_ORDER)) {
-            return array();
+    public function init(array $formData): void
+    {
+        foreach ($this->request as $parameter => $parameterType) {
+            $this->request[$parameter] = $this->formField($formData, $parameter);
+
+            if (self::PARAMETER_REQUIRED === $parameterType && null === $this->request[$parameter]) {
+                throw $this->exception('Required parameter is missing.');
+            }
         }
-        return array_map('strtoupper', $matches[1]);
-    }
 
-    public static function decode_header($header) {
-        $matches = null;
-
-        /* Repair instances where two encodings are together and separated by a space (strip the spaces) */
-        $header = preg_replace(self::rfc2047header_spaces, "$1$2", $header);
-
-        /* Now see if any encodings exist and match them */
-        if (!preg_match_all(self::rfc2047header, $header, $matches, PREG_SET_ORDER)) {
-            return $header;
+        if ($this->token !== $this->request['token']) {
+            throw $this->exception('Forbidden.');
         }
-        foreach ($matches as $header_match) {
-            list($match, $charset, $encoding, $data) = $header_match;
-            $encoding = strtoupper($encoding);
-            switch ($encoding) {
-                case 'B':
-                    $data = base64_decode($data);
-                    break;
-                case 'Q':
-                    $data = quoted_printable_decode(str_replace("_", " ", $data));
-                    break;
-                default:
-                    throw new Exception("preg_match_all is busted: didn't find B or Q in encoding $header");
-            }
-            // This part needs to handle every charset
-            switch (strtoupper($charset)) {
-                case "UTF-8":
-                    break;
-                case 'ISO-8859-2':
-                    $data = iconv('ISO-8859-2', 'UTF-8', $data);
-                    break;
-                default:
-                    /* Here's where you should handle other character sets! */
-                    throw new Exception("Unknown charset in header - time to write some code.");
-            }
-            $header = str_replace($match, $data, $header);
+
+        if (null === $this->request['since']) {
+            $this->request['since'] = (new DateTimeImmutable('3 days ago'))->format('j F Y');
         }
-        return $header;
-    }
-}
 
-function response($payload) {
-    header('Content-Type: application/json');
-    echo json_encode($payload);
-    exit;
-}
-
-function errorResponse($error) {
-    response([
-        'success' => false,
-        'error' => $error,
-    ]);
-}
-
-$mailbox = isset($_POST['mailbox']) ? $_POST['mailbox'] : null;
-$user = isset($_POST['user']) ? $_POST['user'] : null;
-$password = isset($_POST['password']) ? $_POST['password'] : null;
-$subject = isset($_POST['subject']) ? $_POST['subject'] : null;
-$token = isset($_POST['token']) ? $_POST['token'] : null;
-$since = isset($_POST['since']) ? $_POST['since'] : (new DateTimeImmutable('3 days ago'))->format('j F Y');
-$before = isset($_POST['before']) ? (' BEFORE "' . $_POST['before'] . '"') : '';
-
-
-if (null === $mailbox || null === $user || null === $password || null === $token || null === $subject) {
-    errorResponse('Wrong parameter values.');
-}
-
-if ($token !== AUTH_TOKEN) {
-    errorResponse('Wrong auth token.');
-}
-
-$mailbox = imap_open($mailbox,$user,$password);
-
-if (false === $mailbox) {
-    errorResponse(imap_last_error());
-}
-
-$MC = imap_check($mailbox);
-
-$messageUids = imap_search($mailbox, 'SUBJECT "' . $subject . '" SINCE "' . $since . '"' . $before, SE_UID);
-
-$result = [
-    'success' => true,
-    'since' => $since,
-    'before' => $before,
-    'messages' => [],
-];
-
-function findFirstHTMLPath($parts, $parentPath = []) {
-    if (isset($parts)) {
-        foreach ($parts as $key => $part) {
-            $part = (array) $part;
-
-            if (isset($part['parts'])) {
-                $parentPath[] = $key + 1;
-                $path = findFirstHTMLPath($part['parts'], $parentPath);
-
-                if (null !== $path) {
-                    return $path;
-                }
-            }
-
-            if ($part['subtype'] === 'HTML') {
-                $parentPath[] = $key + 1;
-                return $parentPath;
-            }
+        if (null === $this->request['before']) {
+            $this->request['before'] = '';
         }
     }
 
-    return null;
-}
+    public function returnResponse()
+    {
+        $mailbox = $this->mailbox();
+        $messages = [];
 
-foreach ($messageUids as $messageUid) {
-    $structure = (array) imap_fetchstructure($mailbox, $messageUid, FT_UID);
-    $htmlContent = '';
+        foreach ($this->messageNumbers($mailbox, $this->criteria()) as $messageNumber) {
+            $rawHeaders = imap_fetchheader($mailbox, $messageNumber);
+            $messages[] = [
+                'headers' => imap_rfc822_parse_headers($rawHeaders),
+                'raw_headers' => $rawHeaders,
+                'content' => $this->messageContent($mailbox, $messageNumber),
+            ];
+        }
 
-    if (isset($structure['parts'])) {
-        $path = findFirstHTMLPath($structure['parts'], []);
+        imap_close($mailbox);
 
-        if (null === $path) {
+        $this->jsonResponse([
+            'success' => true,
+            'messages' => $messages,
+        ]);
+    }
+
+    public function returnErrorResponse(Exception $exception)
+    {
+        $this->jsonResponse([
+            'success' => false,
+            'error' => $exception->getMessage(),
+        ]);
+    }
+
+    private function messageContent($mailbox, $messageNumber): string
+    {
+        $structure = (array)imap_fetchstructure($mailbox, $messageNumber);
+
+        if (isset($structure['parts'])) {
+            $path = $this->findFirstHTMLPath($structure['parts'], []);
+
+            if (null === $path) {
+                $section = '1';
+            } else {
+                $section = join('.', $path);
+            }
+        } else {
             $section = '1';
         }
-        else {
-            $section = join('.', $path);
+
+        $content = imap_fetchbody($mailbox, $messageNumber, $section);
+
+        if (empty($content)) {
+            $content = imap_fetchbody($mailbox, $messageNumber, '1.' . $section);
         }
+
+        return $this->html2text($content);
     }
-    else {
-        $section = '1';
+
+    private function html2text($htmlContent)
+    {
+        $content = quoted_printable_decode($htmlContent);
+        $content = iconv('ISO-8859-2', 'UTF-8', $content);
+        $content = str_replace('&nbsp;', " ", $content);
+        $content = stripslashes($content);
+        $content = html_entity_decode($content);
+        $content = str_replace('</td>', "|", $content);
+        $content = str_replace('<br>', "\n", $content);
+        $content = strip_tags($content);
+
+        return $content;
     }
 
-        $htmlContent = imap_fetchbody($mailbox, $messageUid, $section, FT_UID);
+    private function criteria(): string
+    {
+        $criteria = [
+            'SUBJECT' => $this->request['subject'],
+            'SINCE' => $this->request['since'],
+        ];
 
-        if (empty($htmlContent)) {
-            $htmlContent = imap_fetchbody($mailbox, $messageUid, '1.' . $section, FT_UID);
+        if (!empty($this->request['before'])) {
+            $criteria['BEFORE'] = $this->request['before'];
         }
 
-    $rawContent = $htmlContent;
+        return implode(' ', array_map(function ($name, $value) {
+            return $name . ' "' . $value . '"';
+        }, array_keys($criteria), $criteria));
+    }
 
-    $htmlContent = quoted_printable_decode($htmlContent);
-    $htmlContent = iconv('ISO-8859-2', 'UTF-8', $htmlContent);
-    $htmlContent = str_replace('&nbsp;', " ", $htmlContent);
-    $htmlContent = stripslashes($htmlContent);
-    $htmlContent = html_entity_decode($htmlContent);
-    $htmlContent = str_replace('</td>', "|", $htmlContent);
-    $htmlContent = str_replace('<br>', "\n", $htmlContent);
-    $htmlContent = strip_tags($htmlContent);
+    private function mailbox()
+    {
+        $mailbox = imap_open(
+            $this->request['mailbox'],
+            $this->request['user'],
+            $this->request['password']);
 
-    $header = imap_headerinfo($mailbox, imap_msgno($mailbox, $messageUid));
-
-    $message = [
-        'date' => null,
-        'from' => null,
-        'subject' => null,
-        'section' => $section,
-        'structure' => $structure,
-        'content' => $htmlContent,
-    ];
-
-    if (false !== $header) {
-        $header = (array) $header;
-
-        if (isset($header['date'])) {
-            $message['date'] = $header['date'];
+        if (false === $mailbox) {
+            throw $this->exception(imap_last_error());
         }
-        if (isset($header['from'])) {
-            $message['from'] = $header['fromaddress'];
-        }
-        if (isset($header['subject'])) {
-            try {
-                $message['subject'] = mail::decode_header($header['subject']);
+
+        return $mailbox;
+    }
+
+    private function messageNumbers($mailbox, string $criteria): array
+    {
+        return imap_search($mailbox, $criteria);
+    }
+
+    private function jsonResponse($payload)
+    {
+        header('Content-Type: application/json');
+        echo json_encode($payload);
+        exit;
+    }
+
+    private function exception($error): Exception
+    {
+        return new Exception($error);
+    }
+
+    private function formField(array $formData, string $fieldName)
+    {
+        return isset($formData[$fieldName]) ? $formData[$fieldName] : null;
+    }
+
+    private function findFirstHTMLPath($parts, $parentPath = [])
+    {
+        if (isset($parts)) {
+            foreach ($parts as $key => $part) {
+                $part = (array)$part;
+
+                if (isset($part['parts'])) {
+                    $parentPath[] = $key + 1;
+                    $path = $this->findFirstHTMLPath($part['parts'], $parentPath);
+
+                    if (null !== $path) {
+                        return $path;
+                    }
+                }
+
+                if ($part['subtype'] === 'HTML') {
+                    $parentPath[] = $key + 1;
+                    return $parentPath;
+                }
             }
-            catch (Exception $exception) {
-                $message['subject'] = $header['subject'];
-                $message['subject_error'] = 'could not decode header';
-            }
-
         }
-    }
 
-    $result['messages'][] = $message;
+        return null;
+    }
 }
 
-$result['messages'] = array_reverse($result['messages']);
+$controller = new Controller(AUTH_TOKEN);
 
-imap_close($mailbox);
-
-response($result);
+try {
+    $controller->init($_POST);
+    $controller->returnResponse();
+} catch (Exception $exception) {
+    $controller->returnErrorResponse($exception);
+}
